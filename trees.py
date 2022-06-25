c22f3b0f7de81e24bedd06a9aede5b4ddd355678
Chaitanya Sarath <sarat.cherry@gmail.com>
2022-06-25 16:22:43 +0800

    binary tree implementation
    binary tree implementation

---
+++ trees.py
@@ -0,0 +1,39 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def insert(self,value):
+            if(value < self.data):
+                if(self.left != None):
+                    self.left.insert(value)
+                else:
+                    self.left = Node(value)
+            else:
+                if(self.right != None):
+                    self.right.insert(value)
+                else:
+                    self.right = Node(value)
+
+    def traverseTree(self):
+         if(self.left != None):
+             self.left.traverseTree()
+         print(self.data)
+         if(self.right != None):
+             self.right.traverseTree()
+
+
+if __name__ == "__main__":
+
+    node = Node(10)
+
+    node.insert(5)
+    node.insert(15)
+    node.insert(8)
+
+    node.traverseTree()
+
+
+    print(node.right.data)
