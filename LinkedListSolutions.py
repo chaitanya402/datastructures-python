93ae4bde1f51d41693e53fb7bc9b47cd189f8c82
Chaitanya Sarath <sarat.cherry@gmail.com>
2022-07-17 18:39:55 +0800

    Update LinkedListSolutions.py

--- LinkedListSolutions.py
+++ LinkedListSolutions.py
@@ -103,4 +103,36 @@ class Solution:
                 return False
             left = left.next
             right = right.next
-        return True    
+        return True   
+    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        resultList = ListNode()
+        ref = resultList
+        if(list1 is None and list2 is None):
+            return ref.next
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        if list1.val <= list2.val:
+            resultList = list1
+            list1 = list1.next
+        else:
+            resultList = list2
+            list2 = list2.next
+        ref = resultList
+        while list1 is not None and list2 is not None :
+            if list1.val <= list2.val:
+                ref.next = list1
+                ref = ref.next
+                list1 = list1.next
+            else:
+                ref.next = list2
+                ref = ref.next
+                list2 = list2.next
+        if list1 is None:
+            ref.next = list2
+        else:
+            ref.next = list1
+        return resultList
+    
