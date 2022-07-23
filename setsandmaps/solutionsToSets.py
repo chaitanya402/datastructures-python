c1a089c5703a919e74feae069ac6493207e31768
Chaitanya Sarath <sarat.cherry@gmail.com>
2022-07-24 02:16:35 +0800

    added chnages for adding optional clause

--- setsandmaps/solutionsToSets.py
+++ setsandmaps/solutionsToSets.py
@@ -15,3 +15,21 @@ class Solution:
                 if i not in solution: solution.add(i)
 
         return solution
+
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+
+        while (n not in visit):
+            visit.add(n)
+
+            mod, rem, sum = 0, n, 0
+            while (rem > 0):
+                mod = rem % 10
+                sum = sum + (mod * mod)
+                rem = rem // 10
+                print(rem, mod, sum)
+            print(sum)
+            if sum == 1: return True
+            n = sum
+
+        return False
